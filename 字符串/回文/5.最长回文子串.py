3bcc794fe382afdf4527ea8dfce26a0d9f2c5866
CLORISDEE HE <34578082+MisakaMikoto96@users.noreply.github.com>
2019-03-01 16:30:54 +0800

    Update 5.最长回文子串.py

--- 字符串/回文/5.最长回文子串.py
+++ 字符串/回文/5.最长回文子串.py
@@ -16,4 +16,22 @@
 输出: "bb"
 '''
 
+def longestPalindrome( s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        palindrome = []
+        res = []
+        if len(s)>1000:
+            return 'out of limitation'
+        for i in range(len(s)):
+            for j in range(len(s)-i):
+                palindrome.append(s[i:i+j+1]) #所有子串
+        for str in palindrome:
+            if str == str[::-1] and len(str)>2: #判断是否为回文，长度是否大于2
+                res.append(str)
+                res.sort(key=lambda str:len(str))
+        return res[len(res)-1]
 
+#list index out of range T.T
